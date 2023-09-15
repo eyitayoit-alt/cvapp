1d499c23bc90c540aa5f0675979c9b8790215d76
eyitayoit <eyitayoit@gmail.com>
2023-09-15 12:54:43 +0100

    Update to App.js, Cvdisplay.js, Cvedit.js Use React Contex

--- Cvdisplay.js
+++ Cvdisplay.js
@@ -1,38 +1,96 @@
-import React,{useState} from 'react';
-import {Text, Image,Button,View,TextInput,StyleSheet} from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StatusBar } from 'expo-status-bar';
+
+import React,{useState,useEffect,useContext} from 'react';
+import {Text,Linking,Button,View,TextInput,StyleSheet} from 'react-native';
+import { CVContext } from './App';
 
 const CVDisplay=({navigation})=>{
-    const [cvdisplay,setCV]=useState({})
-    const getCV=()=>{
-   AsyncStorage.multiGet(['fname','slackname','github','bio'],(err,result)=>{
-     if(err){
-      console.log(err)
-     }
-     else{
-      setCV(result)
-     }
-   })
-    }
-    getCV()
-    return (
-      <View style={styles.container}>
-    <Text>Welcome to CV Display</Text>
-    <Text>{cvdisplay.fname}</Text>
-    <Button    onPress={()=>{
-      navigation.navigate('AddCV')
-    }} title='Edit CV'/>
+  
+  const cvContent=useContext(CVContext)
+ 
+  
+  return (
+    <View style={styles.container}>
+
+  <Text style={styles.title}>CV</Text>
+   <View style={styles.cvshow}>
+  <Text style={styles.textstyle}>Fullname:{cvContent.fname}</Text>
+  <Text style={styles.textstyle}>Slackname:{cvContent.slackname}</Text>
+  <Text style={styles.textstyle} onPress={()=>{Linking.openURL(`${cvContent.github}`)}}>Github Profile: {cvContent.github}</Text>
+  <Text style={styles.textstyle}>Bio:{cvContent.bio}</Text>
+  </View>
+  <View style={styles.butcontainer}>
+  <Button  style={styles.buttonstyle}  onPress={()=>{
+    navigation.navigate('AddCV')}} title='Edit CV'color={'#f01d1d'}/>
     </View>
-    )
+  </View>
+  )
+  
+}
+export default CVDisplay
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection:'column',
+    backgroundColor: '#fff',
+    width:'100%',
+    justifyContent:'start',
+    paddingLeft:20,
+    paddingRight:20,
+    rowGap:20,
     
-  }
-  export default CVDisplay
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'start',
-    },
+
    
-  });
+  },
+  input: {
+    height: 40,
+    marginTop:12,
+    marginBottom:12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  cvedit:{
+    justifyContent:'start',
+    backgroundColor: '#fff',
+    padding:10,
+  },
+  cvshow:{
+    alignContent:'center',
+
+    width:'90%',
+    marginTop:10,
+    marginLeft:20,
+    gap:30,
+   
+   
+  },
+  textstyle:{
+     fontSize:20,
+  },
+ 
+  title:{
+    marginTop:20,
+    textAlign:'center',
+    fontSize:30,
+    fontWeight:'bold',
+    color:'#f01d1d'
+  },
+  butcontainer:{
+    width:'90%',
+    marginTop:20,
+    marginLeft:'auto',
+    marginRight:'auto'
+    
+  },
+  stylesuccess:{
+    color:'green',
+  },
+  stylerror:{
+    color:'red'
+  },
+  screenstyle:{
+    backgroundColor:'#f01d1d',
+    color:'#fff'
+  }
+
+});
--- Cvedit.js
+++ Cvedit.js
@@ -1,48 +1,126 @@
-import React,{useState} from 'react';
-import {Text, Image,Button,View,TextInput,StyleSheet,AsyncStorage} from 'react-native';
- 
- 
+import { StatusBar } from 'expo-status-bar';
+
+import React,{useState,useEffect,useContext} from 'react';
+import {Text,ScrollView,Button,View,TextInput,StyleSheet} from 'react-native';
+import { CVContext } from './App';
+
 const CVEdit=({navigation})=>{
-const[details,setDetails]=useState({'fname':'','slackname':' ','github':' ','bio':''})
- 
-  const submitCV=()=>{
-   AsyncStorage.multiSet(details.entries(),(err)=>{
-    if(err){
-      console.log(err)
-    }
-    else{
-      console.log('Succesful')
-    }
-   })
-   
-  }
- return(
-  <>
-  <View style={styles.container}>
-  <Text >Welcome to CV edit</Text>
-  </View>
-  <View style={styles.cvedit}>
-   
-    <TextInput   style={styles.input} 
-     onChangeText={(value)=>{setDetails({...details,['fname']:value}) }}  value={details.fname}  placeholder='Name' />
+    const cvContent=useContext(CVContext)
+    
+    
+    
+     return(
+    
+      <ScrollView style={styles.scrollviewstyle}>
+        <View>
+      <Text style={styles.title}>Edit CV</Text>
+      </View>
+      <View style={styles.cvedit}>
+         <Text style={styles.textstyle}>Fullname</Text>
+        <TextInput   style={styles.input} 
+         onChangeText={(value)=>{cvContent.setName(value) }}  value={cvContent.fname}  placeholder='Name' />
+         <Text style={styles.textstyle}>Slackname</Text>
+        <TextInput  
+        style={styles.input}    
+        onChangeText={(value)=>{cvContent.setSlackName(value)}} value={cvContent.slackname}  placeholder='Slackname' />
+    <Text style={styles.textstyle}>Github Profile</Text>
     <TextInput  
     style={styles.input}    
-    onChangeText={(value)=>{setDetails({...details,['slackname']:value})  }} value={details.slackname}  placeholder='Slackname' />
-<TextInput  
-style={styles.input}    
-onChangeText={(value)=>{setDetails({...details,['github']:value}) }}  value={details.github}  placeholder='Github' />
-<TextInput   
-multiline={true}
-numberOfLines={10}
-maxLength={40} style={styles.input}    
-onChangeText={(value)=>{setDetails({...details,['bio']:value}) }}  
-value={details.bio}  
-placeholder='Bio' />
+    onChangeText={(value)=>{cvContent.setGithub(value)}}  value={cvContent.github}  placeholder='Github' />
+    <Text style={styles.textstyle}>Bio</Text>
+    <TextInput  style={styles.textAreaStyle} 
+    multiline={true}
+    numberOfLines={40}
+    maxLength={200}     
+    onChangeText={(value)=>{cvContent.setBio(value)}}  
+    value={cvContent.bio}  
+    placeholder='Bio' />
+    </View>
+    <View style={styles.butcontainer}>
+    <Button   onPress={()=>{ navigation.navigate('Home')}} title='Submit' color={'#f01d1d'}/>
+      </View>
+     </ScrollView> 
+     )
+      
+    }
 
-<Button  style={styles.buttonstyle} onPress={submitCV} title='Submit'/>
-  </View>
-  </>
- )
-  
-}
-export default CVEdit
+    export default CVEdit
+    const styles = StyleSheet.create({
+        container: {
+          flex: 1,
+          flexDirection:'column',
+          backgroundColor: '#fff',
+          width:'100%',
+          justifyContent:'start',
+          paddingLeft:20,
+          paddingRight:20,
+          rowGap:20,
+          
+      
+         
+        },
+        scrollviewstyle:{
+            flex: 1,
+            flexDirection:'column',
+            rowGap:20,
+        },
+        input: {
+          height: 40,
+          marginTop:12,
+          marginBottom:12,
+          borderWidth: 1,
+          padding: 10,
+        },
+        textAreaStyle:{
+            height: 100,
+            marginTop:12,
+            marginBottom:12,
+            borderWidth: 1,
+            padding: 10,
+        },
+        cvedit:{
+          justifyContent:'start',
+          backgroundColor: '#fff',
+          padding:10,
+        },
+        cvshow:{
+          alignContent:'center',
+      
+          width:'90%',
+          marginTop:10,
+          marginLeft:20,
+          gap:30,
+         
+         
+        },
+        textstyle:{
+           fontSize:20,
+        },
+       
+        title:{
+          marginTop:20,
+          textAlign:'center',
+          fontSize:30,
+          fontWeight:'bold',
+          color:'#f01d1d'
+        },
+        butcontainer:{
+          width:'90%',
+          marginTop:20,
+          marginLeft:'auto',
+          marginRight:'auto'
+          
+        },
+        stylesuccess:{
+          color:'green',
+        },
+        stylerror:{
+          color:'red'
+        },
+        screenstyle:{
+          backgroundColor:'#f01d1d',
+          color:'#fff'
+        }
+      
+      });
+      
